4f382e50c2ec5db36cf0a64e9a26fbefd004f571
Rowan Littlefield <rowanlittlefield@ymail.com>
2020-09-21 20:02:05 -0400

    Implement fix for bug in which a single incorrect input would prevent all future input

--- src/controller.rs
+++ src/controller.rs
@@ -6,6 +6,8 @@ pub fn get_user_input() -> UserInput {
   let mut is_valid_input = false;
 
   while !is_valid_input {
+    input = String::new();
+
     io::stdin()
       .read_line(&mut input)
       .expect("Failed to read line");
@@ -25,7 +27,6 @@ pub fn get_user_input() -> UserInput {
 
 fn map_input(input: &String) -> Option<UserInput> {
   let input_str = input.as_str();
-  println!("input_str {}", input_str);
   match input_str {
     "w" => Some(UserInput::UP),
     "d" => Some(UserInput::RIGHT),
